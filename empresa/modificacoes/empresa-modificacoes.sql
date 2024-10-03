aac543874304dc45118ae3664e25c365988d1d94
Luara <luaraperilli@hotmail.com>
2024-10-03 19:59:08 -0300

    Modificações aula 03/10/2024

--- empresa/modificacoes/empresa-modificacoes.sql
+++ empresa/modificacoes/empresa-modificacoes.sql
@@ -56,3 +56,22 @@ ADD COLUMN horas_trabalhadas INT CHECK (horas_trabalhadas > 0);
 -- Atualizar todas as alocações com 8 horas trabalhadas
 UPDATE alocacao 
 SET horas_trabalhadas = 8;
+
+-- Consultas aula 03/10/2024
+SELECT * FROM funcionario;
+
+-- Retorne o nome e o CPF dos funcionários cujo email possui @unifei no endereço
+SELECT nome, identidade, email FROM funcionario
+WHERE email LIKE '%@unifei%';
+
+SELECT * FROM alocacao;
+
+-- Retorne o código dos projetos que o funcionário com um determinado código foi alocado
+SELECT codp FROM alocacao
+WHERE codf = '7';
+
+-- Retorne o código dos projetos e dos funcionários de todas as alocações que iniciaram depois de uma determinada data
+SELECT codp, codf, datai FROM alocacao
+WHERE datai > '2017-01-01'
+ORDER BY datai ASC;
+
